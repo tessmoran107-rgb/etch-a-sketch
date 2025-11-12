efb2de3b2d308ae7525f8a9cbb1b75e9a41f37b3
Tess Moran <231676564+tessmoran107-rgb@users.noreply.github.com>
2025-11-12 12:40:07 +0000

    Added reset button and change grid size functionality

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let green = document.querySelector(".greenBtn");
 let purple = document.querySelector(".purpleBtn");
 let erase = document.querySelector(".eraseBtn");
 let reset = document.querySelector(".resetBtn");
+let refresh = document.querySelector(".refreshBtn");
+let refreshWidth = document.querySelector("#width");
+let refreshHeight= document.querySelector("#height");
+
+let colour = "";
+
+black.addEventListener("click", () => { colour = "black"; })
+red.addEventListener("click", () => { colour = "red"; })
+green.addEventListener("click", () => { colour = "green"; })
+purple.addEventListener("click", () => { colour = "purple"; })
+erase.addEventListener("click", () => { colour = ""; })
 
 
 function createGrid(x, y) {
@@ -18,35 +29,33 @@ function createGrid(x, y) {
 
         i--;
 
-        let colour = "";
-
-        black.addEventListener("click", () => {
-            colour = "black";
-        })
-        
-        red.addEventListener("click", () => {
-            colour = "red";
-        })
-
-        green.addEventListener("click", () => {
-            colour = "green";
-        })
-
-        purple.addEventListener("click", () => {
-            colour = "purple";
-        })
-
-        erase.addEventListener("click", () => {
-            colour = "";
-        })
-
         square.addEventListener("mouseover", (event) => {
             event.target.style.backgroundColor = colour;
         })
-    }    
-}
+    }  
+} 
 
 createGrid(16, 16);
+
+reset.addEventListener("click", () => {
+    let squares = document.querySelectorAll(".sketchpad div");
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    });
+    colour = "";
+});
+
+
+refresh.addEventListener("click", () => { 
+    document.querySelector(".sketchpad").innerHTML = "";
+
+    let newWidth = Number(refreshWidth.value);
+    let newHeight = Number(refreshHeight.value);
+
+    createGrid(newWidth, newHeight);
+})
+
+
 
 
 
